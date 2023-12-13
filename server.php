9490b0c15647820959eb8a093c9c5bb330613da2
Isah-Hamza <hamzolium@gmail.com>
2023-12-13 06:17:29 +0100

    -

--- server.php
+++ server.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Create the 'blogs' table if it doesn't exist
         $createTableQuery = "CREATE TABLE IF NOT EXISTS blogs (
                                 id INT AUTO_INCREMENT PRIMARY KEY,
-                                title VARCHAR(255) NOT NULL,
+                                title VARCHAR(255) NOT NULL UNIQUE,
                                 body VARCHAR(2000) NOT NULL,
                                 author VARCHAR(255) NULL,
                                 created_date VARCHAR(255) NULL,
@@ -37,21 +37,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         // Insert data into the database
-        $query = "INSERT INTO blogs (title, body, author, image_url, created_at,) VALUES ('$title', '$body','$author','$image_url', '$created_at')";
+        $query = "INSERT INTO blogs (title, body, author, image_url, created_at) VALUES ('$title', '$body','$author','$image_url', '$created_at')";
 
         try {
             $result = mysqli_query($connection, $query);
 
             if ($result) {
-                $userId = mysqli_insert_id($connection); // Retrieve the last inserted id
+                $blogId = mysqli_insert_id($connection); // Retrieve the last inserted id
                 $response = [
-                    'message' => 'Waitlist user created successfully.',
-                    'userId' => $userId,
+                    'message' => 'One blog item created successfully.',
+                    'blogId' => $blogId,
                 ];
                 http_response_code(200);
             } else {
                 $response = [
-                    'error' => 'An error occurred while creating the waitlist user.',
+                    'error' => 'An error occurred while creating the blog item.',
                 ];
                 http_response_code(500);
             }
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Check if it's a duplicate entry error
             if ($ex->getCode() == 1062) {
                 $response = [
-                    'error' => 'Email address is already in use. Please use a different email.',
+                    'error' => 'A blog with this same title already exists. Please use a different title.',
                 ];
                 http_response_code(400);
             } else {
@@ -73,8 +73,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Handle GET requests (retrieve all data)
 
-    // Retrieve all data from the 'subscribers' table
-    $query = "SELECT * FROM subscribers";
+    // Retrieve all data from the 'blogs' table
+    $query = "SELECT * FROM blogs";
     $result = mysqli_query($connection, $query);
 
     if ($result) {
